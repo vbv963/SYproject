a679a7cbc05102e5a12dc9f746fbd95e20a36e25
vbv963 <vbv963@gmail.com>
2023-09-14 10:53:24 +0900

    Feat: "Generic 싱글톤 구현"
    일반 싱글톤과 Generic 싱글톤 구현 후 Controller통해 접근

---
+++ SY project/Assets/Script/SampleController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Singleton.Instance.InscreaseScore(10);
+        GameManager.Instance.InscreaseScore(15);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
