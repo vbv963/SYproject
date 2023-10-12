905ea7659e46e6fbc896e7eafe0aa5d7f50b2216
vbv963 <vbv963@gmail.com>
2023-10-12 11:31:44 +0900

    Feat : "타워 Target 발사 까지"

---
+++ SY project/Assets/Scripts/TowerSystem/Projectile.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Rigidbody rb;      //내부에서서 사용할 rb
+
+    public float moveSpeed;         //이동속도
+    public float damagedAmount;     //데미지 량 
+    private bool hasDamaged;        //flag 선언  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * moveSpeed;          //rb의 앞쪽 방향으로 총알 이동 속도를 입력
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy" && !hasDamaged)
+        {
+            hasDamaged = true;
+        }
+
+        Destroy(gameObject);                                  //Trigger 충돌이 일어나면 파괴 
+    }
+}
